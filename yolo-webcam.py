15acaf3e2b8c0ef42c9f5d2e75bb83b60f4f0c57
Achmad Dino Saputra <achmadino2015@gmail.com>
2024-03-31 22:21:05 +0700

    add class name in bbox

--- yolo-webcam.py
+++ yolo-webcam.py
@@ -9,6 +9,17 @@ cap.set(4, 720)
 
 model = YOLO("model/yolov8n.pt")
 
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
 
 while True:
   success, img = cap.read()
@@ -29,6 +40,11 @@ while True:
 
       # menambahkan confidence pada bbox
       conf = math.ceil((box.conf[0] * 100)) / 100
-      cvzone.putTextRect(img, f'{conf}', (max(0, x1), max(35, y1)))
+
+      # menambahkan classname
+      cls = int(box.cls[0])
+
+      # menabahkan text ke dalam rect
+      cvzone.putTextRect(img, f'{classNames[cls]} {conf}', (max(0, x1), max(35, y1)), scale=1, thickness=1)
   cv2.imshow("Image", img)
   cv2.waitKey(1)
