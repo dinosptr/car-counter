ad4cdbb8373ec1b399d61c529aa806d4c2d8c8b4
Achmad Dino Saputra <achmadino2015@gmail.com>
2024-03-31 21:10:28 +0700

    add predict with webcam

---
+++ yolo-webcam.py
@@ -0,0 +1,29 @@
+from ultralytics import YOLO
+import cv2
+import cvzone
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 720)
+cap.set(4, 720)
+
+model = YOLO("model/yolov8n.pt")
+
+
+while True:
+  success, img = cap.read()
+  results = model(img)
+
+  for result in results:
+    boxes = result.boxes
+    for box in boxes:
+      x1, y1, x2, y2 = box.xyxy[0]
+      x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+
+      # cara pertama menggunakan library opencv (cv2)
+      # cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+
+      # Cara kedua menggunakan cvzone
+      w, h = x2-x1, y2-y1
+      cvzone.cornerRect(img, (x1, y1, w, h))
+  cv2.imshow("Image", img)
+  cv2.waitKey(1)
